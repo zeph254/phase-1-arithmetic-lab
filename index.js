c7a874c776e10ab1d8d0aadcc1fc4d58dda51a90
zephania owuor <ularezephaniah@gmail.com>
2024-09-27 08:04:09 +0300

    arithmetic-lab

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+let num1=31
+let num2=2
+let multiply=(num1 * num2)
+console.log(multiply);
 
-// Write your code here
+let random = Math.floor(Math.random()*10)+1;
+
+let num3=34
+let num4=5
+let mod=(num3 % num4)
+console.log(mod);
+
+let  numbers=(2,5,20)
+let max = Math.max(numbers)
+console.log(numbers);
